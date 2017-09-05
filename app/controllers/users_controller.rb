2cdced394f26d047c6743ca89ff31f7aafcad3a5
landroide13 <lazaro02@hotmail.com>
2017-09-05 10:59:00 +0700

    edit articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -28,6 +32,10 @@ class UsersController < ApplicationController
     else
      render 'new'
     end
+  end
+
+  def show
+    @user = User.find(params[:id])    
   end
 
 
