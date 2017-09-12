498c28398de38c2d6fa680d29bfda972951e1e7d
landroide13 <lazaro02@hotmail.com>
2017-09-12 10:38:45 +0700

    Categories Listing

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -7,14 +7,22 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
-      post_via_redirect categories_path, category: {name: "sports"}
-    end
+      post categories_path, params: { category: {name: "sports"}}
+      follow_redirect!
+    end 
     assert_template 'categories/index'
     assert_match 'sports', response.body
   end  
 
-
-
-
+  test "invaild category submission result in failure " do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name: " "}}
+    end
+    assert_template 'categories/new'
+    assert_select 'h2'
+    assert_select 'p.mb-0'
+  end
 
 end
