1bff724bce811bec9c4f3cb97dcde3a30859d3a0
landroide13 <lazaro02@hotmail.com>
2017-09-13 12:39:20 +0700

    Set categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: {minimum:4, maximum:9}
   validates :description, presence: true,length: {minimum:5, maximum:250}
   validates :user_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909032242) do
+ActiveRecord::Schema.define(version: 20170913050633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
